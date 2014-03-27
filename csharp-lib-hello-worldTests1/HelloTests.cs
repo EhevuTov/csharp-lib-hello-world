4ed84b713d36e7153637548c693afafb89eaca10
James Gosnell <jamesgosnell@gmail.com>
2014-03-27 01:45:52 -0500

    ADDED test project

---
+++ csharp-lib-hello-worldTests1/HelloTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp_lib_hello_world;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace csharp_lib_hello_world.Tests
+{
+    [TestClass()]
+    public class HelloTests
+    {
+        [TestMethod()]
+        public void HelloTest()
+        {
+            // arrange
+            string expected = "Hello, World!";
+            string actual;
+            Phrase hello = new Phrase();
+
+            // act
+            actual = hello.returnPhrase();
+            
+            // assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
