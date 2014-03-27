01ba7ec36481cd8ec7f03eea0361097a99a00099
ehevutov <jamesgosnell@gmail.com>
2014-03-26 19:47:22 -0500

    INITIAL code commit

---
+++ csharp-lib-hello-world/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lib_hello_world
+{
+    public class Hello
+    {
+        private string phrase;
+        public Hello(){
+            phrase = "Hello, World!";
+        }
+        public string returnPhrase()
+        {
+            return phrase;
+        }
+        public void sayPhrase()
+        {
+            Console.WriteLine(phrase);
+        }
+        public void setPhrase(string n){
+            phrase = n;
+        }
+    }
+}
